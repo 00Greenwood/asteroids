62609b5462bca8af343b63959059e531295fe699
00Greenwood <marcus_greenwood@hotmail.co.uk>
2025-05-24 21:10:14 +0200

    Allow player to rotate

--- player.py
+++ player.py
@@ -16,9 +16,15 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def update(self, dt):
-        # Update player position or state here if needed
-        pass
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
